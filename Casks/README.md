0cf30e1773b6cd8802e8aa65e1631f0f022e77ce
miyonaka <dev@miyonaka.com>
2020-08-10 18:41:11 +0900

    init commit

---
+++ Casks/README.md
@@ -0,0 +1,8 @@
+# homebrew-oneclickdl-for-microsd
+
+## Usage
+
+```bash
+$ brew tap miyonaka/oneclickdl-for-microsd
+$ brew cask install oneclickdl-for-microsd
+```
---
+++ Casks/oneclickdl-for-microsd.rb
@@ -0,0 +1,10 @@
+cask "oneclickdl-for-microsd" do
+  version :latest
+  sha256 :no_check
+
+  url "http://www.e-comtec.co.jp/1click/OneclickDLForMicroSD.zip"
+  name "Oneclick DL For MicroSD"
+  homepage "http://www.e-comtec.co.jp/1click/"
+
+  app "ワンクリックDL for microSD.app"
+end
